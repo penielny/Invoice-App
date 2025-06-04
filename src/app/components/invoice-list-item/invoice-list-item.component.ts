8910a0e2f830d624141f4a261ee0fe6a9bff0374
penielny <penielnyinaku@gmail.com>
2025-06-04 23:15:42 +0000

    feat: enhance invoice management with new components, routing, and styling

--- src/app/components/invoice-list-item/invoice-list-item.component.ts
+++ src/app/components/invoice-list-item/invoice-list-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Invoice } from '../../interfaces/invoice';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-list-item',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './invoice-list-item.component.html',
   styleUrl: './invoice-list-item.component.scss'
 })
---
+++ src/app/pages/new-invoice/new-invoice.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-invoice',
+  imports: [],
+  templateUrl: './new-invoice.component.html',
+  styleUrl: './new-invoice.component.scss'
+})
+export class NewInvoiceComponent {
+
+}
